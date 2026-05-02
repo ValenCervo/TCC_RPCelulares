d05208307d057f719398d0bf29baff8b0f9c66cc
valentina cervo <valentinacervo883@gmail.com>
2026-05-02 13:55:46 -0300

    Login e Cadastro (revisar cadastro)

--- Sistema/routes/index.js
+++ Sistema/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Você está em login' })
+});
+
+router.get('/cadastro', function(req, res, next) {
+  res.render('cadastro', { title: 'Você está no cadastro' })
+});
+
 module.exports = router;
